1f892df2e4f55aa9877640d61caeb61f4fbd421b
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-07-04 01:46:42 +0000

    almost completed task, but still have to figure out hover

--- src/js/component/Container.jsx
+++ src/js/component/Container.jsx
@@ -2,29 +2,55 @@ import React, { useState } from 'react';
 
 export const Container = () => {
     const [ inputValue, setInputValue ] = useState('');
+    const [ allTasks, setAllTasks ] = useState([]);
+
+    const handleInputEnter = (event) => {
+        // let newField = { text: event.target.value };
+        if(event.key == 'Enter'){
+            setAllTasks([...allTasks, event.target.value]);
+            setInputValue('');
+        }
+    }
 
     return (
         <div className="row gx-0">
             <div className='paper-stack'>
-                <input 
-                className='input-field shadows-into-light-regular'
-                type="text"
-                placeholder="What needs to be done?"
-                onChange={
-                    (event) => setInputValue(event.target.value)
-                }
-                value={inputValue}
-                />
-                <div className='footer shadows-into-light-regular'>X items left</div>
+                <div>
+                    <input 
+                        className='input-field  border-0 mb-0 shadows-into-light-regular'
+                        type="text"
+                        placeholder="What needs to be done?"
+                        value={inputValue}
+                        onChange={
+                            (event) => setInputValue(event.target.value)
+                        }
+                        onKeyDown={(event) => handleInputEnter(event)}
+                    />
+                </div>
+                
+                {allTasks.map((input, index) => {
+                    const removeItems = (index) => {
+                        let data = [...allTasks];
+                        data.splice(index, 1);
+                        setAllTasks(data);
+                    };
+                    return(
+                        <div key={index}>
+                            <ul className="input-field task shadows-into-light-regular p-0">
+                                {allTasks[index]}
+                                <button 
+                                    className='button shadows-into-light-regular'
+                                    onClick={() => removeItems(index)}
+                                >
+                                    X
+                                </button>
+                            </ul>
+                            
+                        </div>
+                    )
+                })}
+                <div className='footer shadows-into-light-regular'>{allTasks.length} items left</div>
             </div>
-            
         </div>
-
     );
 };
-
-const AnyComponent = () => {
-    const [ inputValue, setInputValue ] = useState('');
-    
-    return <input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} />
-}
